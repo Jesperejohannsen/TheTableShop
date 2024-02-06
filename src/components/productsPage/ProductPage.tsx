0a912042ab12d29799bf4e70b3bb27c63aa61d11
Jesperejohannsen <you@example.com>
2024-02-06 19:53:24 +0100

    adding new design

--- src/components/productsPage/ProductPage.tsx
+++ src/components/productsPage/ProductPage.tsx
@@ -2,31 +2,39 @@ import React from "react";
 import "./ProductPage.css";
 import WoodTableLivingRoom from "../../assets/woodtableinlivingroom.jpg";
 
-const ProductPage: React.FC = () => {
+interface ProductProps {
+  image: string;
+  name: string;
+  price: string;
+  description: string;
+}
 
-  const products = Array(6).fill(null);
+const Product: React.FC<ProductProps> = ({ image, name, price, description }) => (
+  <div className="productItem">
+    <div className="productImageContainer">
+      <img src={image} alt={name} className="productImage" />
+    </div>
+    <div className="productDetails">
+      <h1 className="productName">{name}</h1>
+      <p className="productPrice">{price}</p>
+      <p className="productDescription">{description}</p>
+      <button className="addToCartButton">Add to Cart</button>
+    </div>
+  </div>
+);
+
+const ProductPage: React.FC = () => {
+  const products = Array(6).fill({
+    image: WoodTableLivingRoom,
+    name: "Handcrafted Wood Table",
+    price: "$299.99",
+    description: "Experience the elegance and durability of our handcrafted wood table, made with the finest materials to add a touch of nature to your home."
+  });
 
   return (
     <div className="productsContainer">
-      {products.map((_, index) => (
-        <div key={index} className="productItem">
-          <div className="productImageContainer">
-            <img
-              src={WoodTableLivingRoom}
-              alt="Handcrafted Wood Table"
-              className="productImage"
-            />
-          </div>
-          <div className="productDetails">
-            <h1 className="productName">Handcrafted Wood Table</h1>
-            <p className="productPrice">$299.99</p>
-            <p className="productDescription">
-              Experience the elegance and durability of our handcrafted wood table,
-              made with the finest materials to add a touch of nature to your home.
-            </p>
-            <button className="addToCartButton">Add to Cart</button>
-          </div>
-        </div>
+      {products.map((product, index) => (
+        <Product key={index} {...product} />
       ))}
     </div>
   );
