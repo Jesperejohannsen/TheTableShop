ca52bfd49eaf37313253187a4c1c0022ff9cebfd
Jesperejohannsen <you@example.com>
2024-02-11 15:58:03 +0100

    changing text from enlighs to danish

--- src/components/aboutPage/AboutPage.tsx
+++ src/components/aboutPage/AboutPage.tsx
@@ -5,11 +5,11 @@ import PlaceholderImage from "../../assets/logo.png";
 const AboutPage: React.FC = () => {
   return (
     <div className="aboutContainer">
-      <h1 className="aboutTitle">About Us</h1>
+      <h1 className="aboutTitle">Om The Table Shop</h1>
       <div className="aboutSection">
         <img src={PlaceholderImage} alt="Owner" className="aboutImage" />
         <div>
-          <h2 className="aboutSubtitle">The Owner</h2>
+          <h2 className="aboutSubtitle">Thomas Thers Segato</h2>
           <p className="aboutText">
             The owner of TTS, John Doe, has a passion for handcrafted wood furniture. With years of experience in the industry, he prides himself on his attention to detail and commitment to customer satisfaction.
           </p>
@@ -18,7 +18,7 @@ const AboutPage: React.FC = () => {
       <div className="aboutSection">
         <img src={PlaceholderImage} alt="Products" className="aboutImage" />
         <div>
-          <h2 className="aboutSubtitle">Our Products</h2>
+          <h2 className="aboutSubtitle">Vores produkter</h2>
           <p className="aboutText">
             We offer a wide range of high-quality, handcrafted wood furniture. Our team of skilled artisans uses the finest materials to create pieces that add a touch of nature to your home.
           </p>
@@ -27,7 +27,7 @@ const AboutPage: React.FC = () => {
       <div className="aboutSection">
         <img src={PlaceholderImage} alt="Sustainability" className="aboutImage" />
         <div>
-          <h2 className="aboutSubtitle">Sustainability</h2>
+          <h2 className="aboutSubtitle">Bæredygtighed</h2>
           <p className="aboutText">
             We are committed to sustainability. We source our materials responsibly and strive to minimize our environmental impact.
           </p>
